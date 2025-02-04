b5d068f94b9bc6cfc15ad2947cab17cbce9318e5
carlos henrique <carlos.hrq.rodrigues@gmail.com>
2025-02-04 15:28:00 -0300

    wip layout

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -2,8 +2,6 @@ import {Sidebar} from "@/components/sidebar";
 
 export default function Dashboard() {
     return (
-        <div className={"min-h-screen bg-background font-sans"}>
             <Sidebar />
-        </div>
     )
 }
--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -1,13 +1,16 @@
 import { Route, Routes } from "react-router";
 import Index from "./index.tsx";
 import AboutPage from "./about";
+import {DashboardLayout} from "@/layout/dashboard";
 import Dashboard from "@/pages/dashboard.tsx";
 
 export default function AppRoutes() {
   return(
     <Routes>
       <Route index element={<Index />} />
-      <Route path={"/dashboard"} element={<Dashboard />} />
+      <Route element={<DashboardLayout />}>
+          <Route index path={"dashboard"} element={<Dashboard />}/>
+      </Route>
       <Route path="about" element={<AboutPage />} />
     </Routes>
   )
